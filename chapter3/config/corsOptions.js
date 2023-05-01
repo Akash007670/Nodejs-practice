7c9b02f26ecee822b6e3fde98e273ceb852c68d7
Akash kumar <akaysharma894@gmail.com>
2023-05-02 01:09:30 +0530

    Created Employee controller and corsOption config

---
+++ chapter3/config/corsOptions.js
@@ -0,0 +1,13 @@
+const whiteList = ["http://localhost:3300/"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true); //null is usually the error and true is supposed to allowed to pass to cors from the given whitelist
+    } else {
+      callback(new Error("Not Allowed By CORS"));
+    }
+  },
+  optionsSuccessStatus: 200,
+};
+
+module.exports = corsOptions;
--- chapter3/routes/api/employees.js
+++ chapter3/routes/api/employees.js
@@ -1,33 +1,21 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
-const data = {};
-
-data.employees = require("../../data/employees.json");
+const {
+  getAllEmployees,
+  createNewEmployee,
+  updateEmployee,
+  deleteEmployee,
+  getSpecificEmployee,
+} = require("../../controllers/employeeController");
 
 router
   .route("/")
-  .get((req, res) => {
-    res.json(data.employees);
-  })
-  .post((req, res) => {
-    res.json({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-    });
-  })
-  .put((req, res) => {
-    res.json({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-    });
-  })
-  .delete((req, res) => {
-    res.json({ id: req.body.id });
-  });
+  .get(getAllEmployees)
+  .post(createNewEmployee)
+  .put(updateEmployee)
+  .delete(deleteEmployee);
 
-router.route("/:id").get((req, res) => {
-  res.json({ id: req.params.id });
-});
+router.route("/:id").get(getSpecificEmployee);
 
 module.exports = router;
